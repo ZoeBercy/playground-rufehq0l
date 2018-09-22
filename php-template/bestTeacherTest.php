3b19f6889214cac4edbb446d4e30d8e4a2ceb87d
michwii <freezerhm@yahoo.fr>
2018-09-22 22:11:25 +0200

    Update bestTeacherTest.php

--- php-template/bestTeacherTest.php
+++ php-template/bestTeacherTest.php
@@ -1,1 +1,14 @@
 
+<?php
+    include 'bestTeacher.php';
+    function test() {
+        if (quiEstLeMeilleurProf() === "Le prof de programmation Web") {
+            echo("TECHIO> success true \r\n");
+        } 
+        else {
+            echo("TECHIO> success false  \r\n");
+            echo("TECHIO> message --channel Oops! Try Again 🐞 \r\n");
+        }
+    }
+    test();
+?>
