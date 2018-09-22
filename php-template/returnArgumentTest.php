5e2cc113558f76d5cc49566e06b3dec89bfa1221
Elyes Hachem <freezerhm@yahoo.fr>
2018-09-22 22:40:41 +0200

    'fgd'

--- php-template/returnArgumentTest.php
+++ php-template/returnArgumentTest.php
@@ -1,9 +1,9 @@
 <?php
-    include 'returnArgument.php';
+    include 'empty.php';
     function test() {
         if (jeRetourneMonArgument("abc") === "abc" && jeRetourneMonArgument(5677) === 5677 && jeRetourneMonArgument(true) === true) {
             echo("TECHIO> success true \r\n");
-        } 
+        }
         else {
             echo("TECHIO> success false  \r\n");
             echo("TECHIO> message --channel Oops! Try Again 🐞 \r\n");
