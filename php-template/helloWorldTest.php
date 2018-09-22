9485d4e02f1e614582a7bcb4ede3cdd3cc25028e
michwii <freezerhm@yahoo.fr>
2018-09-22 22:00:17 +0200

    Update helloWorldTest.php

--- php-template/helloWorldTest.php
+++ php-template/helloWorldTest.php
@@ -1,1 +1,13 @@
-
+<?php
+    include 'helloWorld.php';
+    function test() {
+        if (helloWorld() === "Hello World!") {
+            echo("TECHIO> success true \r\n");
+        } 
+        else {
+            echo("TECHIO> success false  \r\n");
+            echo("TECHIO> message --channel Oops! Try Again 🐞 \r\n");
+        }
+    }
+    test();
+?>
