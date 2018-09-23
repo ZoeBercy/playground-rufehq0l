3cfdba630974cbbf388f5c4119b3d01a213bb037
Elyes Hachem <freezerhm@yahoo.fr>
2018-09-23 19:58:00 +0200

    user filtering

--- php-template/libraryToInclude.php
+++ php-template/libraryToInclude.php
@@ -4,41 +4,11 @@
     }
 
     function getAllUtilisateurs(){
-      $toReturn =  json_decode('[
-        {
-          name : "Nicolas Sarkozy",
-          age: 55,
-          blocked: false,
-          email : "nicolasdu92@gmail.com"
-        },
-        {
-          name : "Francois Hollande",
-          age: 50,
-          blocked: false,
-          email : "fifihollande@gmail.com"
-        },
-        {
-          name : "Disiz la peste",
-          age: 30,
-          blocked: false,
-          email : "disiz@gmail.com"
-        },
-        {
-          name : "IAM",
-          age: 30,
-          blocked: false,
-          email : "iam@gmail.com"
-        },
-        {
-          name : "Youssoupha",
-          age: 30,
-          blocked: false,
-          email : "youyou@gmail.com"
-        }
-      ]');
-
-      echo $toReturn[0]->name."heeloooo";
-
-      return $toReturn;
+      $user1 = (object) array('name' => 'Nicolas Sarkozy', 'age' => 55, 'blocked' => false, 'email' => 'nicolasdu92@gmail.com');
+      $user2 = (object) array('name' => 'Francois Hollande', 'age' => 55, 'blocked' => false, 'email' => 'nicolasdu92@gmail.com');
+      $user3 = (object) array('name' => 'Disiz la peste', 'age' => 55, 'blocked' => false, 'email' => 'nicolasdu92@gmail.com');
+      $user4 = (object) array('name' => 'IAM', 'age' => 55, 'blocked' => false, 'email' => 'nicolasdu92@gmail.com');
+      $user5 = (object) array('name' => 'Youssoupha', 'age' => 55, 'blocked' => false, 'email' => 'nicolasdu92@gmail.com');
+      return [$user1, $user2, $user3, $user4, $user5];
     }
 ?>
