bec5bd5535bcfcd6121694a6c54bd5bdfc6f60ef
Elyes Hachem <freezerhm@yahoo.fr>
2018-09-23 18:28:04 +0200

    date

--- php-template/whichDateTest.php
+++ php-template/whichDateTest.php
@@ -2,7 +2,7 @@
     include 'empty.php';
     function test() {
 
-        if (quelleDate() === date('D')."/".(date("M")-1)."/".date('Y')) {
+        if (quelleDate() === date('d')."/".(date("m")-1)."/".date('Y')) {
             echo("TECHIO> success true \r\n");
         }
         else {
