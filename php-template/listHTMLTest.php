228ace2b9c65e274ae0016876f0b0132feab4a22
Elyes Hachem <freezerhm@yahoo.fr>
2018-09-23 17:29:42 +0200

    kf

--- php-template/listHTMLTest.php
+++ php-template/listHTMLTest.php
@@ -13,12 +13,12 @@
         }
         else {
             echo("TECHIO> success false  \r\n");
-            echo(" value : " + listHTML("Capital",["Paris", "Berlin", "Moscou"]) === "<h3>Capitale</h3><ul><li>Paris</li><li>Berlin</li><li>Moscou</li></ul>");
-            echo(" value : " + listHTML("Burger",["BigMac", "Fish", "Beacon"]) === "<h3>Burger</h3><ul><li>BigMac</li><li>Fish</li><li>Beacon</li></ul>");
-            echo(" value : " + listHTML("Burger",["BigMac"]) === "<h3>Burger</h3><ul><li>BigMac</li></ul>");
-            echo(" value : " + listHTML("",["BigMac"]) === null);
-            echo(" value : " + listHTML(null,["BigMac"]) === null);
-            echo(" value : " + listHTML("truc",[]) === null);
+            echo(" value : " . listHTML("Capital",["Paris", "Berlin", "Moscou"]) === "<h3>Capitale</h3><ul><li>Paris</li><li>Berlin</li><li>Moscou</li></ul>");
+            echo(" value : " . listHTML("Burger",["BigMac", "Fish", "Beacon"]) === "<h3>Burger</h3><ul><li>BigMac</li><li>Fish</li><li>Beacon</li></ul>");
+            echo(" value : " . listHTML("Burger",["BigMac"]) === "<h3>Burger</h3><ul><li>BigMac</li></ul>");
+            echo(" value : " . listHTML("",["BigMac"]) === null);
+            echo(" value : " . listHTML(null,["BigMac"]) === null);
+            echo(" value : " . listHTML("truc",[]) === null);
             echo("TECHIO> message --channel Oops! Try Again 🐞 \r\n");
         }
     }
