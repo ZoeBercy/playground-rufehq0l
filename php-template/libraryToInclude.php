9e0df1a5bed4305d6bc33c86e023df63bc1c747f
Elyes Hachem <freezerhm@yahoo.fr>
2018-09-23 20:03:59 +0200

    user filtering

--- php-template/libraryToInclude.php
+++ php-template/libraryToInclude.php
@@ -6,10 +6,13 @@
     function getAllUtilisateurs(){
       return [
         (object) array('name' => 'Nicolas Sarkozy', 'age' => 55, 'blocked' => false, 'email' => 'nicolasdu92@gmail.com'),
+        (object) array('name' => 'Justin Bieber', 'age' => 16, 'blocked' => false, 'email' => 'justin@gmail.com'),
         (object) array('name' => 'Francois Hollande', 'age' => 55, 'blocked' => false, 'email' => 'fifi@gmail.com'),
+        (object) array('name' => 'Donald Trump', 'age' => 55, 'blocked' => true, 'email' => 'rocketman@gmail.com'),
         (object) array('name' => 'Disiz la peste', 'age' => 55, 'blocked' => false, 'email' => 'disizcestpasdurapdepd@gmail.com'),
         (object) array('name' => 'IAM', 'age' => 55, 'blocked' => false, 'email' => 'meilleurgroupederapfrancais@gmail.com'),
-        (object) array('name' => 'Youssoupha', 'age' => 55, 'blocked' => false, 'email' => 'youyou@gmail.com')
+        (object) array('name' => 'Youssoupha', 'age' => 55, 'blocked' => false, 'email' => 'youyou@gmail.com'),
+        (object) array('name' => 'Le Pen', 'age' => 80, 'blocked' => false, 'email' => null)
       ];
     }
 ?>
